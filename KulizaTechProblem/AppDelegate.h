56dad8b67fe47a7dd97efbdada1e4725c2fdc35f
Pankaj Sharma <Pankaj_Sharma@Trimble.com>
2015-04-11 15:52:23 +0530

    Initial Commit

---
+++ KulizaTechProblem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KulizaTechProblem
+//
+//  Created by Pankaj Sharma on 11/04/15.
+//  Copyright (c) 2015 Pankaj Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KulizaTechProblem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KulizaTechProblem
+//
+//  Created by Pankaj Sharma on 11/04/15.
+//  Copyright (c) 2015 Pankaj Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
