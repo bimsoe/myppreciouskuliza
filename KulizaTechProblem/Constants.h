072bc6bab9281ce8de4ecffea65a06af40d87543
Pankaj Sharma <Pankaj_Sharma@Trimble.com>
2015-04-11 16:12:50 +0530

    Just Started Up

---
+++ KulizaTechProblem/Constants.h
@@ -0,0 +1,30 @@
+//
+//  Constants.h
+//  KulizaTechProblem
+//
+//  Created by Pankaj Sharma on 11/04/15.
+//  Copyright (c) 2015 Pankaj Sharma. All rights reserved.
+//
+
+#ifndef KulizaTechProblem_Constants_h
+#define KulizaTechProblem_Constants_h
+
+#pragma mark - Color Defines
+#define BROWNISH_COLOR  [UIColor colorWithRed:191.0f green:102.0f blue:41.0f alpha:1.0f]
+#define WHITISH_COLOR   [UIColor colorWithRed:239.0f green:239.0f blue:239.0f alpha:1.0f]
+#define PINKISH_COLOR   [UIColor colorWithRed:247.0f green:236.0f blue:229.0f alpha:1.0f]
+#define YELLOWISH_COLOR [UIColor colorWithRed:244.0f green:244.0f blue:232.0f alpha:1.0f]
+#define GRAYISH_COLOR   [UIColor colorWithRed:187.0f green:187.0f blue:187.0f alpha:1.0f]
+
+#pragma Font Colors
+#define DARK_BLACK_COLOR        [UIColor blackColor]
+#define LIGHT_BLACK_COLOR       [UIColor colorWithRed:102.0f green:102.0f blue:102.0f alpha:1.0f]
+#define VERY_LIGHT_BLACK_COLOR  [UIColor colorWithRed:153.0f green:153.0f blue:153.0f alpha:1.0f]
+
+#pragma mark - Image Defines
+#define BUY_NOW_IMG     @"buy-now"
+#define CALL_US_IMG     @"call-us"
+#define LEFT_ARROW_IMG  @"left_arrow"
+#define RIGHT_ARROW_IMG @"right_arrow"
+
+#endif
--- KulizaTechProblem/ViewController.m
+++ KulizaTechProblem/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "Constants.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,7 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  [self.view setBackgroundColor:WHITISH_COLOR];
 }
 
 - (void)didReceiveMemoryWarning {
